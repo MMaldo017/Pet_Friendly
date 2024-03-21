71b88adeebcbce2cb635dd57ba94db1adc978e6e
Joseph Lang <langj6757@gmail.com>
2024-03-21 23:30:30 +0000

    Change redirect to new homepage route upon logout

--- ghi/src/components/LoginButton.jsx
+++ ghi/src/components/LoginButton.jsx
@@ -55,7 +55,7 @@ const LoginButton = () => {
             setTimeout(() => {
                 setButtonVisible(true)
             }, 4000)
-            navigate('/')
+            navigate('/pet-friendly')
         } catch (error) {
             console.error('Log out failed', error)
         }
